64efd073945e80f8818bc32402cb720b64ddaacb
saroj-rumsan <saroj.shrestha@rumsan.net>
2024-04-03 14:21:41 +0545

    Initial commit

---
+++ apps/c2c/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'c2c',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/c2c'
+};
