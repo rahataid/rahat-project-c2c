b10bbc78952f9fbc438a5136e076d24866938a4c
Raghav Kattel <raghav.kattel@rumsan.net>
2024-04-23 18:19:11 +0545

    Update react-query configuration and provider

--- libs/react-query/README.md
+++ libs/react-query/README.md
@@ -1,11 +1,11 @@
-# subgraph-query
+# react-query
 
 This library was generated with [Nx](https://nx.dev).
 
 ## Building
 
-Run `nx build subgraph-query` to build the library.
+Run `nx build react-query` to build the library.
 
 ## Running unit tests
 
-Run `nx test subgraph-query` to execute the unit tests via [Jest](https://jestjs.io).
+Run `nx test react-query` to execute the unit tests via [Jest](https://jestjs.io).
--- libs/react-query/src/subgraph/subgraph.provider.tsx
+++ libs/react-query/src/subgraph/subgraph.provider.tsx
@@ -1,18 +1,39 @@
 import { Client } from '@urql/core';
-import React from 'react';
+import React, {
+  FC,
+  useState,
+  createContext,
+  useContext,
+  Dispatch,
+  SetStateAction,
+} from 'react';
 
 type C2CSubgraphContextType = {
   subgraphClient: Client;
-  setSubgraphClient: React.Dispatch<React.SetStateAction<Client>>;
+  setSubgraphClient: Dispatch<SetStateAction<Client>>;
 };
 
-const C2CSubgraphContext = React.createContext<C2CSubgraphContextType>(null);
+const C2CSubgraphContext = createContext<C2CSubgraphContextType | undefined>(
+  undefined
+);
 
-const C2CSubgraphProvider = ({ children }) => {
-  const [subgraphClient, setSubgraphClient] = React.useState<Client>();
+type C2CSubgraphProviderProps = {
+  children: React.ReactNode;
+};
+
+const C2CSubgraphProvider: FC<C2CSubgraphProviderProps> = ({ children }) => {
+  const [subgraphClient, setSubgraphClient] = useState<Client>();
 
   return (
-    <C2CSubgraphContext.Provider value={{ subgraphClient, setSubgraphClient }}>
+    <C2CSubgraphContext.Provider
+      value={{
+        subgraphClient: subgraphClient as Client,
+
+        setSubgraphClient: setSubgraphClient as Dispatch<
+          SetStateAction<Client>
+        >,
+      }}
+    >
       {children}
     </C2CSubgraphContext.Provider>
   );
@@ -21,7 +42,7 @@ const C2CSubgraphProvider = ({ children }) => {
 export default C2CSubgraphProvider;
 
 export const useC2CSubgraph = (): C2CSubgraphContextType => {
-  const context = React.useContext(C2CSubgraphContext);
+  const context = useContext(C2CSubgraphContext);
   if (context === undefined) {
     throw new Error('useC2CSubgraph must be used within a C2CSubgraphProvider');
   }
