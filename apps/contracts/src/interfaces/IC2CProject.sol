b684f69c79aebec7398afb03a25f214cbecc0f1b
manjik-rumsan <manjik.shrestha@rumsan.com>
2024-06-07 07:59:17 +0545

    updated c2c tests

--- apps/contracts/src/interfaces/IC2CProject.sol
+++ apps/contracts/src/interfaces/IC2CProject.sol
@@ -2,51 +2,56 @@
 pragma solidity 0.8.20;
 
 interface IC2CProject {
-    /**
-     * @dev Emitted when a claim is processed.
-     * @param _beneficiary The address of the beneficiary.
-     * @param _tokenAddress The address of the token.
-     * @param _amount The amount of tokens processed.
-     */
-    event TransferProcessed(
-        address indexed _beneficiary,
-        address indexed _tokenAddress,
-        uint _amount
-    );
+  /**
+   * @dev Emitted when a claim is processed.
+   * @param _tokenAddress The address of the token.
+   * @param _to The address of the beneficiary.
+   * @param _from The address of the donor.
+   * @param _amount The amount of tokens processed.
+   */
+  event TransferProcessed(
+    address indexed _tokenAddress,
+    address indexed _to,
+    address indexed _from,
+    uint _amount
+  );
 
-    /**
-     * @dev Checks if the given address is a donor.
-     * @param _address The address to check.
-     * @return A boolean value indicating whether the address is a donor or not.
-     */
-    function isDonor(address _address) external returns (bool);
+  /**
+   * @dev Checks if the given address is a donor.
+   * @param _address The address to check.
+   * @return A boolean value indicating whether the address is a donor or not.
+   */
+  function isDonor(address _address) external returns (bool);
 
-    /**
-     * @dev Allows the contract to accept tokens from a specified address.
-     * @param _from The address from which the tokens are being transferred.
-     * @param _amount The amount of tokens being transferred.
-     */
-    function acceptToken(
-        address _from,
-        address _tokenAddress,
-        uint256 _amount
-    ) external;
+  /**
+   * @dev Allows the contract to accept tokens from a specified address.
+   * @param _from The address from which the tokens are being transferred.
+   * @param _amount The amount of tokens being transferred.
+   */
+  function acceptToken(
+    address _from,
+    address _tokenAddress,
+    uint256 _amount
+  ) external;
 
-    /**
-     * @dev Allows the user to withdraw tokens from the contract.
-     * @param _tokenAddress The address of the token to be withdrawn.
-     */
-    function withdrawToken(address _tokenAddress) external;
+  /**
+   * @dev Allows the user to withdraw tokens from the contract.
+   * @param _tokenAddress The address of the token to be withdrawn.
+   */
+  function withdrawToken(
+    address _tokenAddress,
+    address _withdrawAddress
+  ) external;
 
-    /**
-     * @dev Processes a transfer of tokens to a beneficiary.
-     * @param _beneficiary The address of the beneficiary.
-     * @param _tokenAddress The address of the token to be transferred.
-     * @param _amount The amount of tokens to be transferred.
-     */
-    function processTransferToBeneficiary(
-        address _beneficiary,
-        address _tokenAddress,
-        uint256 _amount
-    ) external;
+  /**
+   * @dev Processes a transfer of tokens to a beneficiary.
+   * @param _beneficiary The address of the beneficiary.
+   * @param _tokenAddress The address of the token to be transferred.
+   * @param _amount The amount of tokens to be transferred.
+   */
+  function disburseProjectToken(
+    address _tokenAddress,
+    address _beneficiary,
+    uint256 _amount
+  ) external;
 }
--- apps/contracts/src/interfaces/IRahatDonor.sol
+++ apps/contracts/src/interfaces/IRahatDonor.sol
@@ -1,29 +1,15 @@
 //SPDX-License-Identifier: LGPL-3.0
-pragma solidity 0.8.20;
+pragma solidity ^0.8.17;
 
 import '@openzeppelin/contracts/utils/introspection/IERC165.sol';
 
 interface IRahatDonor is IERC165 {
-    function createToken(
-        string memory _name,
-        string memory _symbol,
-        uint8 decimals
-    ) external;
+  function createToken(string memory _name, string memory _symbol, uint8 decimals) external;
 
-    function mintToken(address _token, uint256 _amount) external;
+  function mintToken(address _token, uint256 _amount) external;
 
-    function mintTokenAndApprove(
-        address _token,
-        address _approveAddress,
-        uint256 _amount
-    ) external;
+  function mintTokenAndApprove(address _token, address _approveAddress, uint256 _amount) external;
 
-    function mintTokenAndApprove(
-        address _token,
-        address _approveAddress,
-        uint256 _amount,
-        string memory _description
-    ) external;
+  function addTokenOwner(address _token, address _ownerAddress) external;
 
-    function addTokenOwner(address _token, address _ownerAddress) external;
 }
